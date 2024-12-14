3aac2a447a9d9c2c22ec7f674a3f71358040e83f
shakti-25 <thevarshakti02@gmail.com>
2024-12-14 15:59:46 +0530

    Update untitled9.py

--- untitled9.py
+++ untitled9.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1txzLN9CJJdMnRV0efVWnoPUcYgpw0H5U
 """
 
-!pip install streamlit==1.28.0
+
 
 import streamlit as st
 import pickle
